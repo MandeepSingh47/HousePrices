2916cb0a23b6d5a2c3a00ea9f79360791411cb0f
Mandeep <ms230994@gmail.com>
2020-01-28 14:27:50 -0500

    Added Techniques

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from sklearn import metrics, ensemble
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.svm import SVR
@@ -10,12 +11,11 @@ from sklearn.svm import SVR
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
-
 # Reshaping train and test data
 total = train.isnull().sum().sort_values(ascending=False)
 percent = (train.isnull().sum() / train.isnull().count()).sort_values(ascending=False)
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-print(missing_data)
+
 train = train.drop((missing_data[missing_data['Total'] > 81]).index, 1)
 total_test = test.isnull().sum().sort_values(ascending=False)
 percent_test = (test.isnull().sum() / test.isnull().count()).sort_values(ascending=False)
@@ -45,11 +45,13 @@ test['BsmtFinSF1'] = test['BsmtFinSF1'].fillna(test['BsmtFinSF1'].mean())
 test['TotalBsmtSF'] = test['TotalBsmtSF'].fillna(test['TotalBsmtSF'].mean())
 test['GarageArea'] = test['GarageArea'].fillna(test['GarageArea'].mean())
 test['GarageCars'] = test['GarageCars'].fillna(test['GarageCars'].mean())
-print(train.shape)
 
-print(test.shape)
-x_train, x_test, y_train, y_test = train_test_split(train, train,
+# print(train)
+# print(test)
+
+x_train, x_test, y_train, y_test = train_test_split(train.drop('SalePrice', axis=1), train['SalePrice'],
                                                     test_size=0.2, random_state=1)
+
 y_train = y_train.values.reshape(-1, 1)
 y_test = y_test.values.reshape(-1, 1)
 
@@ -60,13 +62,47 @@ x_test = sc_x.fit_transform(x_test)
 y_train = sc_x.fit_transform(y_train)
 y_test = sc_y.fit_transform(y_test)
 
-# linear = LinearRegression()
-# linear.fit(x_train, y_train)
-# predictions = linear.predict(x_test)
-# predictions = predictions.reshape(-1, 1)
+linear = LinearRegression()
+linear.fit(x_train, y_train)
+lnr_predictions = linear.predict(x_test)
+lnr_predictions = lnr_predictions.reshape(-1, 1)
 
-# svr = SVR(kernel='rbf')
-# svr.fit(x_train, np.ravel(y_train))
-# svr_pred = svr.predict(x_test)
-# svr_pred = svr.pred.reshape(-1, 1)
-# print(svr_pred)
+print('MAE: for Linear', metrics.mean_absolute_error(y_test, lnr_predictions))
+print('MSE:', metrics.mean_squared_error(y_test, lnr_predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, lnr_predictions)))
+
+GBR = ensemble.GradientBoostingRegressor(n_estimators=500, max_depth=4, min_samples_split=2,
+                                         learning_rate=0.01, loss='ls')
+GBR.fit(x_train, y_train)
+GBR_prediction = GBR.predict(x_test)
+GBR_prediction = GBR_prediction.reshape(-1, 1)
+
+print('MAE: for GBR', metrics.mean_absolute_error(y_test, GBR_prediction))
+print('MSE:', metrics.mean_squared_error(y_test, GBR_prediction))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, GBR_prediction)))
+
+
+svr = SVR(kernel='rbf')
+svr.fit(x_train, np.ravel(y_train))
+predictions = svr.predict(x_test)
+predictions = predictions.reshape(-1, 1)
+print(predictions)
+
+print('MAE for svr:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
+errorrate = np.array([metrics.mean_squared_error(y_test, lnr_predictions), metrics.mean_squared_error(y_test, GBR_prediction), metrics.mean_squared_error(y_test, predictions)])
+plt.figure(figsize=(16, 5))
+plt.plot(errorrate)
+plt.show()
+
+# ID = test['ID']
+# test = pd.DataFrame(ID, columns=['ID'])
+# test = sc_x.fit_transform(test)
+# test_prediction_clf = svr.predict(test)
+# test_prediction_clf = test_prediction_clf.reshape(-1,1)
+# test_prediction_clf = sc_y.inverse_transform(test_prediction_clf)
+# test_prediction_clf = pd.DataFrame(test_prediction_clf, columns=['SalePrice'])
+# result = pd.concat([test, test_prediction_clf], axis=1)
+# result.to_csv('Kaggle_Submission.csv', index=False)
