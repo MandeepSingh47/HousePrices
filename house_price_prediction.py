97d35f7ae4c48303f8b55e3667c1173201f36e18
MandeepSingh47 <54869772+MandeepSingh47@users.noreply.github.com>
2020-01-29 11:37:09 -0500

    Model build

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -5,13 +5,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 from sklearn.svm import SVR
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
-# Reshaping train and test data
+'''Reshaping train and test data'''
 total = train.isnull().sum().sort_values(ascending=False)
 percent = (train.isnull().sum() / train.isnull().count()).sort_values(ascending=False)
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
@@ -22,7 +21,8 @@ percent_test = (test.isnull().sum() / test.isnull().count()).sort_values(ascendi
 missing_data = pd.concat([total_test, percent_test], axis=1, keys=['Total', 'Percent'])
 test = test.drop((missing_data[missing_data['Total'] > 78]).index, 1)
 
-# Reshaping categorical data
+
+'''Reshaping categorical data'''
 categorical_feature_mask = train.dtypes == object
 categorical_cols = train.columns[categorical_feature_mask].tolist()
 categorical_feature_mask_test = test.dtypes == object
@@ -46,15 +46,14 @@ test['TotalBsmtSF'] = test['TotalBsmtSF'].fillna(test['TotalBsmtSF'].mean())
 test['GarageArea'] = test['GarageArea'].fillna(test['GarageArea'].mean())
 test['GarageCars'] = test['GarageCars'].fillna(test['GarageCars'].mean())
 
-# print(train)
-# print(test)
 
 x_train, x_test, y_train, y_test = train_test_split(train.drop('SalePrice', axis=1), train['SalePrice'],
                                                     test_size=0.2, random_state=1)
-
 y_train = y_train.values.reshape(-1, 1)
 y_test = y_test.values.reshape(-1, 1)
 
+
+'''Scaling Up the Data'''
 sc_x = StandardScaler()
 sc_y = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
@@ -62,47 +61,60 @@ x_test = sc_x.fit_transform(x_test)
 y_train = sc_x.fit_transform(y_train)
 y_test = sc_y.fit_transform(y_test)
 
-linear = LinearRegression()
-linear.fit(x_train, y_train)
-lnr_predictions = linear.predict(x_test)
-lnr_predictions = lnr_predictions.reshape(-1, 1)
 
-print('MAE: for Linear', metrics.mean_absolute_error(y_test, lnr_predictions))
-print('MSE:', metrics.mean_squared_error(y_test, lnr_predictions))
-print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, lnr_predictions)))
+'''Linear Regression'''
+# linear = LinearRegression()
+# linear.fit(x_train, y_train)
+# lnr_predictions = linear.predict(x_test)
+# lnr_predictions = lnr_predictions.reshape(-1, 1)
+#
+# print('MAE: for Linear', metrics.mean_absolute_error(y_test, lnr_predictions))
+# print('MSE:', metrics.mean_squared_error(y_test, lnr_predictions))
+# print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, lnr_predictions)))
 
+
+'''Gradient Boosting'''
 GBR = ensemble.GradientBoostingRegressor(n_estimators=500, max_depth=4, min_samples_split=2,
                                          learning_rate=0.01, loss='ls')
-GBR.fit(x_train, y_train)
+GBR.fit(x_train, y_train.ravel())
 GBR_prediction = GBR.predict(x_test)
 GBR_prediction = GBR_prediction.reshape(-1, 1)
 
-print('MAE: for GBR', metrics.mean_absolute_error(y_test, GBR_prediction))
-print('MSE:', metrics.mean_squared_error(y_test, GBR_prediction))
-print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, GBR_prediction)))
+# print('MAE: for GBR', metrics.mean_absolute_error(y_test, GBR_prediction))
+# print('MSE:', metrics.mean_squared_error(y_test, GBR_prediction))
+# print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, GBR_prediction)))
 
 
-svr = SVR(kernel='rbf')
-svr.fit(x_train, np.ravel(y_train))
-predictions = svr.predict(x_test)
-predictions = predictions.reshape(-1, 1)
-print(predictions)
+'''Support Vector Machine'''
+# svr = SVR(kernel='rbf')
+# svr.fit(x_train, np.ravel(y_train))
+# predictions = svr.predict(x_test)
+# predictions = predictions.reshape(-1, 1)
+# print(predictions)
 
-print('MAE for svr:', metrics.mean_absolute_error(y_test, predictions))
-print('MSE:', metrics.mean_squared_error(y_test, predictions))
-print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+# print('MAE for svr:', metrics.mean_absolute_error(y_test, predictions))
+# print('MSE:', metrics.mean_squared_error(y_test, predictions))
+# print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
 
-errorrate = np.array([metrics.mean_squared_error(y_test, lnr_predictions), metrics.mean_squared_error(y_test, GBR_prediction), metrics.mean_squared_error(y_test, predictions)])
-plt.figure(figsize=(16, 5))
-plt.plot(errorrate)
-plt.show()
 
-# ID = test['ID']
-# test = pd.DataFrame(ID, columns=['ID'])
-# test = sc_x.fit_transform(test)
-# test_prediction_clf = svr.predict(test)
-# test_prediction_clf = test_prediction_clf.reshape(-1,1)
-# test_prediction_clf = sc_y.inverse_transform(test_prediction_clf)
-# test_prediction_clf = pd.DataFrame(test_prediction_clf, columns=['SalePrice'])
-# result = pd.concat([test, test_prediction_clf], axis=1)
-# result.to_csv('Kaggle_Submission.csv', index=False)
+'''Comparing the Error Rate Between Different Regressions'''
+# errorrate = np.array([metrics.mean_squared_error(y_test, lnr_predictions), metrics.mean_squared_error(y_test, GBR_prediction), metrics.mean_squared_error(y_test, predictions)])
+# plt.figure(figsize=(16, 5))
+# plt.plot(errorrate)
+# plt.show()
+
+'''Using the test data'''
+testdata = pd.read_csv("test.csv")
+ID = testdata['Id']
+testdata = pd.DataFrame(ID, columns=['Id'])
+test = sc_x.fit_transform(test)
+
+test_prediction = GBR_prediction.predict(test)
+test_prediction = test_prediction.reshape(-1, 1)
+test_prediction = sc_y.inverse_transform(test_prediction)
+test_prediction = pd.DataFrame(test_prediction, columns=['SalePrice'])
+print(test_prediction)
+
+
+result = pd.concat([test, test_prediction], axis=1)
+result.to_csv('Kaggle_Submission.csv', index=False)
